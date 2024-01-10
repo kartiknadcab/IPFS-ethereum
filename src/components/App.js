505d18b94920b95da72c119f4037da4f17e9d8f4
Manthan-Nadcab <manthan.nadcab@gmail.com>
2024-01-10 10:21:25 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,35 +2,41 @@ import "./App.css";
 import { useState } from "react";
 import { create } from "ipfs-http-client";
 
-const client = create("https://ipfs.infura.io:5001/api/v0");
+const client = create("http://127.0.0.1:5002/api/v0");
 
 const App = () => {
-  const [file, setFile] = useState(null);
-  const [urlArr, setUrlArr] = useState([]);
+    const [file, setFile] = useState(null);
+    const [urlArr, setUrlArr] = useState([]);
 
-  const retrieveFile = (e) => {
-    const data = e.target.files[0];
-    const reader = new window.FileReader();
-    reader.readAsArrayBuffer(data);
+    const retrieveFile = (e) => {
 
-    reader.onloadend = () => {
-      setFile(Buffer(reader.result));
+      const data = e.target.files[0];
+      const reader = new window.FileReader();
+      reader.readAsArrayBuffer(data);
+
+      reader.onloadend = () => {
+        setFile(Buffer(reader.result));
+      };
+
+      e.preventDefault();
     };
 
-    e.preventDefault();
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      const created = await client.add(file);
-      const url = `https://ipfs.infura.io/ipfs/${created.path}`;
-      setUrlArr((prev) => [...prev, url]);
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+        console.log("click")
+      try {
+        const created = await client.add(file);
+        const url = `http://127.0.0.1:9090/ipfs/${created.path}`;
+        fetch(url,{
+          "headers":{
+            "Content-Type":"application/json"
+          }
+        }).then(d=>d.json()).then(d=>console.log(d))
+        // setUrlArr((prev) => [...prev, url]);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
 
   return (
     <div className="App">
@@ -38,14 +44,14 @@ const App = () => {
 
       <div className="main">
         <form onSubmit={handleSubmit}>
-          <input type="file" onChange={retrieveFile} />
+          <input type="text" onChange={(e)=>setFile(e.target.value)} />
           <button type="submit" className="button">Submit</button>
         </form>
       </div>
 
       <div className="display">
         {urlArr.length !== 0
-          ? urlArr.map((el) => <img src={el} alt="nfts" />)
+          ? urlArr.map((el,i) => <p  key={i} >{el}</p>)
           : <h3>Upload data</h3>}
       </div>
     </div>
